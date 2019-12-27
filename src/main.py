cd49f5fd7289ecbf2ea6c69ed05e37593cad770a
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2019-12-27 09:24:57 +0100

    Optimise chain generation

--- src/main.py
+++ src/main.py
@@ -14,7 +14,16 @@ L_DIRS = [
     np.array(( 0,  0, -1))
     ]
 L_DIM = 100
-TRIES = 100
+LATTICE = np.zeros((L_DIM, L_DIM, L_DIM))
+TRIES = 1000
+
+def get_lattice_value(coordinate):
+    c = coordinate % L_DIM
+    return LATTICE[tuple(c)]
+
+def set_lattice_value(coordinate, value):
+    c = coordinate % L_DIM
+    LATTICE[tuple(c)] = value
 
 class Chain():
     def __init__(self):
@@ -23,20 +32,24 @@ class Chain():
         
     def add_monomer(self, m):
         self.monomers.append(m)
+        set_lattice_value(m, 1)
         if len(self.monomers) > 1:
             dist = self.monomers[-1] - self.monomers[0]
             self.r_ee_sqr = np.dot(dist, dist)
             
     def new_monomer_energy(self, m):
         energy = 0.
-        for monomer in self.monomers:
-            if (m == monomer).all():
-                return np.inf
+        if get_lattice_value(m):
+            return np.inf
         return energy
             
     def size(self):
         return len(self.monomers)
-        
+    
+    
+def delete_chain(chain):
+    for m in chain.monomers:
+        set_lattice_value(m, 0)
         
         
 def build_chain_rosenbluth(N):
@@ -57,6 +70,7 @@ def build_chain_rosenbluth(N):
         tot_weight = sum(weights)
         if tot_weight == 0.:
             # dead end, we start over
+            delete_chain(new_chain)
             new_chain = Chain()
             last_inserted = np.random.randint(0, L_DIM, 3)
             new_chain.add_monomer(last_inserted)
@@ -88,6 +102,7 @@ for N in range(40, 100, 5):
             last_chain = chain
         t += 1
         r_ee_sqr += chain.r_ee_sqr
+        delete_chain(chain)
     r_ee_sqr /= TRIES - 1
     print(N - 1, file=sys.stderr)
     print(N - 1, r_ee_sqr)
